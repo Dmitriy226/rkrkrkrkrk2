faa13b86d10c70c1650eedecad940d80ea6466dc
Dmitriy226 <skryabikovdr@gmail.com>
2024-12-20 01:17:23 +0300

    Create find_files.sh

---
+++ find_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Проверка, что оба аргумента переданы
+if [ $# -ne 2 ]; then
+    echo "Пожалуйста, укажите директорию и расширение файлов."
+    exit 1
+fi
+
+DIRECTORY=$1
+EXTENSION=$2
+
+# Проверка, существует ли указанная директория
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Директория не найдена."
+    exit 1
+fi
+
+# Поиск файлов с указанным расширением
+FILES=$(find "$DIRECTORY" -type f -name "*.$EXTENSION")
+
+# Проверка, найдены ли файлы
+if [ -z "$FILES" ]; then
+    echo "Файлы с расширением $EXTENSION не найдены."
+else
+    echo "Найденные файлы с расширением $EXTENSION:"
+    echo "$FILES"
+fi
